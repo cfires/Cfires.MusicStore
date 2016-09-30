f33987908548343f913d78662d5e1f3df7a403c9
cfires <caofangzh@163.com>
2016-09-30 17:44:36 +0800

    配置数据库

--- MvcMusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
+++ MvcMusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
@@ -9,6 +9,10 @@ namespace MvcMusicStore.Models
     //扩展 DbContext 基类
     public class MusicStoreEntities : DbContext
     {
+        public MusicStoreEntities() : base("name=MusicStoreEntities")
+        {
+        }
+
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Artist> Artists { get; set; }
